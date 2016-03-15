cd93865321502718099ff0774f3936ec67a83f23
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-15 17:24:55 +0100

    Set up main functionality collection view

--- CollectionViews/ViewController.swift
+++ CollectionViews/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CollectionViewController: UICollectionViewController {
+    
+    var names = ["puppy1", "puppy2", "puppy3", "puppy4", "puppy5"]
+    var images = [UIImage(named: "puppy1.jpg"), UIImage(named: "puppy2.jpg"), UIImage(named: "puppy3.jpg"), UIImage(named: "puppy4.jpg"), UIImage(named: "puppy5.jpg")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        // After setting the tags for the image and label.
+        var image = cell.viewWithTag(1) as! UIImageView
+        image.image = images[indexPath.row]
+        var name = cell.viewWithTag(2) as! UILabel
+        name.text = names[indexPath.row]
+        
+        return cell
     }
 
 
